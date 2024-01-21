09d7a4728f0d28767fde917e67d3667e710b256e
Samuel Letsinger <153053442+Samuel-Letsinger@users.noreply.github.com>
2024-01-21 01:22:57 -0600

    Update ReplaceRemovedBedrock.java

--- ReplaceRemovedBedrock.java
+++ ReplaceRemovedBedrock.java
@@ -1,5 +1,8 @@
 package com.urbigmumz.world;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
@@ -26,24 +29,42 @@ public final class ReplaceRemovedBedrock extends JavaPlugin implements Listener 
     public void onBlockBreak(BlockBreakEvent event) {
         World world = event.getBlock().getWorld();
         if (world.getEnvironment() != Environment.THE_END) {
+            System.out.println("Made it #1");
             fillBedrockAtYZero(world);
         }
     }
 
     public static void fillBedrockAtYZero(World world) {
         Environment environment = world.getEnvironment();
+        World overworld = Bukkit.getServer().getWorld("world");
+        World nether = Bukkit.getServer().getWorld("world_nether");
         if (environment == Environment.NORMAL || environment == Environment.NETHER) {
-            for (int x = 0; x < 16; x++) {
-                for (int z = 0; z < 16; z++) {
-                    Block block = world.getBlockAt(x, 0, z);
-                    if (environment == Environment.NETHER) {
-                        Block block2 = world.getBlockAt(x, 127, z);
-                        if (block2.getType() != Material.BEDROCK) {
-                            block2.setType(Material.BEDROCK);
+            System.out.println("Made it #2");
+            for (Chunk chunk : world.getLoadedChunks()) {
+                int chunkX = chunk.getX() << 4;
+                int chunkZ = chunk.getZ() << 4;
+                for (int x = 0; x < 16; x++) {
+                    for (int z = 0; z < 16; z++) {
+                        Location blockLocation;
+                        Block block;
+                        if (environment == Environment.NETHER) {
+                            blockLocation = new Location(nether, chunkX + x, 0, chunkZ + z);
+                            block = blockLocation.getBlock();
+                        } else {
+                            blockLocation = new Location(overworld, chunkX + x, 0, chunkZ + z);
+                            block = blockLocation.getBlock();
                         }
-                    }
-                    if (block.getType() != Material.BEDROCK) {
-                        block.setType(Material.BEDROCK);
+                        if (environment == Environment.NETHER) {
+                            Location blockLocation2 = new Location(nether, chunkX + x, 217, chunkZ + z);
+                            Block block2 = blockLocation2.getBlock();
+                            if (block2.getType() != Material.BEDROCK) {
+                                block2.setType(Material.BEDROCK);
+                            }
+                        }
+                        if (block.getType() != Material.BEDROCK) {
+                            System.out.println("Made it #3");
+                            block.setType(Material.BEDROCK);
+                        }
                     }
                 }
             }
